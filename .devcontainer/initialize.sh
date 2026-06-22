570a312d3f4ab9b6c74389be5a0a16f8348f8fdf
t-noguchi <t-noguchi@isp.co.jp>
2026-06-22 06:50:28 +0000

    added scaffolding

---
+++ .devcontainer/initialize.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# -----------------------------------------------------------------------------
+# gitignore
+# -----------------------------------------------------------------------------
+# VPN に接続していないとき，devcontainer 内から docker run しても正しい応答がない
+# 場合があるため，WSL 上で実行できるように initializeCommand で実行する
+
+# cf. https://github.com/github/gitignore
+echo "# -----------------------------------------------------------------------------" > .gitignore
+echo "# java" >> .gitignore
+echo "# -----------------------------------------------------------------------------" >> .gitignore
+docker run --rm simonwhitaker/gibo dump Java >> .gitignore
+
+echo "# -----------------------------------------------------------------------------" >> .gitignore
+echo "# maven" >> .gitignore
+echo "# -----------------------------------------------------------------------------" >> .gitignore
+docker run --rm simonwhitaker/gibo dump Maven >> .gitignore
+
+echo "# -----------------------------------------------------------------------------" >> .gitignore
+echo "# node" >> .gitignore
+echo "# -----------------------------------------------------------------------------" >> .gitignore
+docker run --rm simonwhitaker/gibo dump Node >> .gitignore
+
+echo "# -----------------------------------------------------------------------------" >> .gitignore
+echo "# vscode" >> .gitignore
+echo "# -----------------------------------------------------------------------------" >> .gitignore
+docker run --rm simonwhitaker/gibo dump VisualStudioCode >> .gitignore
+echo "!.vscode/*.sh" >> .gitignore
+
+echo "*.tar.gz" >> .gitignore
+
+# .env は git 管理対象とする
+sed -i -e 's/^\.env$/#\.env/g' .gitignore
---
+++ .kiro/steering/tech.md
@@ -0,0 +1,15 @@
+# 技術基盤
+
+## Minecraft
+
+- バージョン: 1.20.1
+- MOD API: Forge
+
+# 開発ツール
+
+- VSCode + DevContainers を利用する
+
+## Java
+
+- バージョン: 17
+- パッケージ管理: Maven
